db296dec100a86b61633d72b2dfe5a4d8b852a65
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2020-10-27 16:30:39 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,24 @@
+class Food {
+    constructor(){
+   
+    }
+
+   updateFoodStock(foodStock){
+    this.foodStock=foodStock;
+   }
+
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+
+   deductFood(){
+     if(this.foodStock>0){
+      this.foodStock=this.foodStock-1;
+     }
+    }
+
+    getFoodStock(){
+      return this.foodStock;
+    }
+
+}
